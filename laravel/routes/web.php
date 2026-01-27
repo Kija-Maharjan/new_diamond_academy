52ff86cf035e1dc733a1cd6f34eeb085cf2e25ab
Swaraj <swarajmaharjan26@gmail.com>
2026-01-27 22:10:10 +0545

    fixed the ui and css

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('ui', [
+        'title' => 'Diamond Academy',
+        'headerTitle' => 'Diamond Academy Documentation'
+    ]);
 });
